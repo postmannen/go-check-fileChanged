57d5505c100f83ebdd1d5385d5bfe430b90d5ec3
Bjørn Tore Svinningen <postmannen@gmail.com>
2018-08-31 09:50:57 +0200

    added initial read of file, and updated comments.

--- cmd/main.go
+++ cmd/main.go
@@ -12,6 +12,7 @@ import (
 
 func main() {
 	updates := make(chan mapfile.Update)
+
 	fw, err := mapfile.New("commandToTemplate.json", updates)
 	if err != nil {
 		log.Println("Main: Failed to create new FileWatcher struct: ", err)
@@ -22,52 +23,30 @@ func main() {
 		log.Println("Main: Failed to Watch: ", err)
 	}
 
+	aMap := make(map[string]string)
+
 	for {
 		select {
 		case u := <-fw.Updates:
-			if err == nil {
-				fmt.Println("No Error: ", u)
+			if u.Err == nil {
+				fmt.Println("No FileWatch Error: ", u)
+
+				aMap, err = fw.Convert(aMap)
+
+				printMap(aMap)
 			} else {
-				fmt.Println("Error: ", u)
+				fmt.Println("FileWatch Error: ", u)
 			}
 		}
 	}
-
-	/*
-		//Create data structure who holds the map and channels
-		d := mapfile.NewData("./commandToTemplate.json")
-
-		//Start the file watcher
-		mapfile.StartFileWatcher(d)
-		defer mapfile.StopFileWatcher()
-
-		for {
-			select {
-			case <-d.FileUpdated:
-				var err error
-				//Convert the file content, and insert into map.
-				//Will return current map if the new one fails.
-				d.AMap, err = mapfile.Convert(d.FileName, d.AMap)
-				if err != nil {
-					log.Println("Main :", err)
-				}
-
-				printMap(d)
-
-			//Catch the errors from the functions that are Go routines
-			case errF := <-d.FileError:
-				log.Println("Error: Main: Received on error channel.", errF)
-			}
-		}
-	*/
 }
 
-func printMap(d mapfile.Data) {
+func printMap(m map[string]string) {
 
 	//Print out all the values for testing
 	fmt.Println("----------------------------------------------------------------")
 	fmt.Println("Content of the map unmarshaled from fileContent :")
-	for key, value := range d.AMap {
+	for key, value := range m {
 		fmt.Println("key = ", key, "value = ", value)
 	}
 	fmt.Println("----------------------------------------------------------------")
--- jsonfiletomap.go
+++ jsonfiletomap.go
@@ -10,7 +10,6 @@ import (
 	"errors"
 	"fmt"
 	"io/ioutil"
-	"log"
 	"os"
 	"time"
 
@@ -30,7 +29,9 @@ type FileWatcher struct {
 	fsWatcher *fsnotify.Watcher
 }
 
-//New create a new FileWatcher struct
+//New creates a new FileWatcher struct.
+//The "updates chan Update" is taken as input so the user
+//can choose to make a buffered or unbuffered channel.
 func New(fileName string, updates chan Update) (*FileWatcher, error) {
 	watcher, err := fsnotify.NewWatcher()
 	if err != nil {
@@ -40,7 +41,8 @@ func New(fileName string, updates chan Update) (*FileWatcher, error) {
 	return &FileWatcher{fileName, updates, watcher}, nil
 }
 
-//Watch will check the file
+//Watch will check the file, and send an update over the
+//methods Updates channel if file is changed.
 func (fw *FileWatcher) Watch() error {
 	err := fw.fsWatcher.Add(fw.name)
 	if err != nil {
@@ -48,6 +50,12 @@ func (fw *FileWatcher) Watch() error {
 	}
 
 	go func() {
+		//Trigger the first reading of the file by sending an update to the Updates channel
+		fw.Updates <- Update{
+			Time: time.Now(),
+			Err:  nil,
+		}
+
 		for {
 			select {
 			case event := <-fw.fsWatcher.Events:
@@ -68,89 +76,14 @@ func (fw *FileWatcher) Close() error {
 	return fw.fsWatcher.Close()
 }
 
-//------------------------------------------------------
-//done is used for stopping the services.
-var done = make(chan bool)
-
-//StartFileWatcher starts the filewatcher.
-func StartFileWatcher(d Data) {
-	//************
-	go checkFileUpdated(d)
-}
-
-//StopFileWatcher is used to stop all running Go routines
-func StopFileWatcher() {
-	done <- true
-}
-
-//Data holds all the variable types needed for the service
-type Data struct {
-	FileUpdated chan bool
-	FileError   chan error
-	FileName    string
-	AMap        map[string]string
-}
-
-//NewData creates a data structure for
-//the variables used in the package
-func NewData(fileName string) Data {
-	return Data{
-		FileUpdated: make(chan bool),
-		FileError:   make(chan error),
-		FileName:    fileName,
-		AMap:        make(map[string]string),
-	}
-
-}
-
-//checkFileUpdated , this is basically the same code as given as example
-//in the fsnotify doc.......with some minor changes.
-func checkFileUpdated(d Data) {
-	watcher, err := fsnotify.NewWatcher()
-	if err != nil {
-		log.Println("Failed fsnotify.NewWatcher")
-		return
-	}
-	defer watcher.Close()
-
-	go func() {
-		//Give a true value to updated so it reads the file the first time.
-		d.FileUpdated <- true
-
-		for {
-			select {
-			case event := <-watcher.Events:
-				//log.Println("event:", event)
-				if event.Op&fsnotify.Write == fsnotify.Write {
-					//log.Println("modified file:", event.Name)
-					//testing with an update chan to get updates
-					//instead of just logs
-					d.FileUpdated <- true
-				}
-			case err := <-watcher.Errors:
-				log.Println("pkg:jsonfiletomap checkFileUpdated:", err)
-				d.FileError <- err
-			}
-		}
-	}()
-
-	err = watcher.Add(d.FileName)
-	if err != nil {
-		d.FileError <- err
-	}
-	<-done
-
-	return
-}
-
 //Convert loads the file,
 //reads it's content, parse the JSON
 //and returns a new map with the parsed values.
 //If it fails at some point then return the current map.
-func Convert(fileName string, currentMap map[string]string) (map[string]string, error) {
+func (fw *FileWatcher) Convert(currentMap map[string]string) (map[string]string, error) {
 	theMap := make(map[string]string)
 
-	f, err := os.Open(fileName)
+	f, err := os.Open(fw.name)
 	if err != nil {
 		e := fmt.Sprintln("Convert: Keeping current map, file open failed :", err.Error())
 		return currentMap, errors.New(e)
